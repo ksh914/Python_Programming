c64faabe474e6c9bb368dfa389b4662457c5527e
ksh914 <87693086+ksh914@users.noreply.github.com>
2022-03-14 16:45:58 +0900

    3.14 commit

--- Python/Baekjoon/test.py
+++ Python/Baekjoon/test.py
@@ -1,9 +1,16 @@
 import sys
-from collections import Counter
-N=int(input())
-ML=[]
+MN=0
+N,K = map(int, sys.stdin.readline().split())
+ML = []
 for i in range(N):
     ML.append(int(sys.stdin.readline()))
     
-ML.sort()
-print(Counter(ML).most_common)
+for j in range(-1,-(N+1),-1):
+    if K==0:
+        break
+    if K>ML[j]:
+        MN +=K//ML[j]
+        K %=ML[j]
+        
+print(MN)
+        
