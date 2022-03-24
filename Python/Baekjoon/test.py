3e5d17a2838c606036f1dc75576ddfadde07f27c
ksh914 <87693086+ksh914@users.noreply.github.com>
2022-03-24 18:08:49 +0900

    3.24

--- Python/Baekjoon/test.py
+++ Python/Baekjoon/test.py
@@ -1,16 +1,2 @@
-import sys
-MN=0
-N,K = map(int, sys.stdin.readline().split())
-ML = []
-for i in range(N):
-    ML.append(int(sys.stdin.readline()))
-    
-for j in range(-1,-(N+1),-1):
-    if K==0:
-        break
-    if K>ML[j]:
-        MN +=K//ML[j]
-        K %=ML[j]
-        
-print(MN)
-        
+A,I=map(int, input().split())
+print(A*(I-1)+1)
